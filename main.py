457b17f3f43be99e3fe6af503c810b30876d54b1
michael-2956 <work.michael.2956@gmail.com>
2023-12-03 00:15:17 +0200

    tasks 3, 4

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import numpy as np
+import soundfile as sf
+from scipy.io import wavfile
 import matplotlib.pyplot as plt
 from scipy.fft import fft, fftshift, fftfreq, ifft
 from scipy.signal import butter, buttord, lfilter, freqz, cheby1, cheb1ord
@@ -304,18 +306,257 @@ def task_2():
 
 
 def task_3():
-    pass
+    duration_s, sample_rate_hz, signal_amp, signal_freq = 10, 128, 1, 50
+    noise_std = 0.01
+
+    time_s = np.linspace(0, duration_s, int(duration_s * sample_rate_hz), endpoint=False)
+    fft_freqs = fftfreq(time_s.shape[0], d=1/sample_rate_hz)
+
+    plt.figure(figsize=(12, 10))
+
+    signal = signal_amp * np.sin(2 * np.pi * signal_freq * time_s)
+
+    plt.subplot(3, 2, 1)
+    plt.plot(time_s, signal)
+    plt.title('Time Domain: 50 Hz Noise')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude (V)')
+    plt.grid(True)
+
+    signal_fft = fft(signal) / time_s.shape[0]
+    signal_amps = np.abs(signal_fft)
+
+    plt.subplot(3, 2, 2)
+    plt.plot(fft_freqs[:len(fft_freqs)//2], signal_amps[:len(signal_amps)//2])
+    plt.title('Amplitude Spectrum: 50 Hz Noise')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.grid(True)
+
+    noise = np.random.normal(0, noise_std, size=time_s.shape)
+
+    plt.subplot(3, 2, 3)
+    plt.plot(time_s, noise)
+    plt.title('Time Domain: Signal')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude (V)')
+    plt.grid(True)
+
+    noise_fft = fft(noise) / time_s.shape[0]
+    noise_amps = np.abs(noise_fft)
+
+    plt.subplot(3, 2, 4)
+    plt.plot(fft_freqs[:len(fft_freqs)//2], noise_amps[:len(noise_amps)//2])
+    plt.title('Amplitude Spectrum: Signal')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.grid(True)
+
+    signal_w_noise = signal + noise
+
+    plt.subplot(3, 2, 5)
+    plt.plot(time_s, signal_w_noise)
+    plt.title('Time Domain: Signal with noise')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude (V)')
+    plt.grid(True)
+
+    signal_w_noise_fft = fft(signal_w_noise) / time_s.shape[0]
+    signal_w_noise_amps = np.abs(signal_w_noise_fft)
+
+    plt.subplot(3, 2, 6)
+    plt.plot(fft_freqs[:len(fft_freqs)//2], signal_w_noise_amps[:len(signal_w_noise_amps)//2])
+    plt.title('Amplitude Spectrum: Signal with noise')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.grid(True)
+
+    plt.savefig("task_3_signals.png")
+    plt.tight_layout()
+    plt.show()
+
+    title, (wp, ws) = "bandstop", ((48.8, 50.6*1.007), (48.8*1.007, 50.6))
+
+    print(f"\n\n\nFilter: {title}")
+    print(f"wp {wp}, ws: {ws}")
+    lowest_butterworth_order, butterworth_natural_frequency = buttord(
+        wp=wp,
+        ws=ws,
+        gpass=3,
+        gstop=20,
+        analog=False, fs=sample_rate_hz
+    )
+    print(f"{lowest_butterworth_order = }")
+    print(f"{butterworth_natural_frequency = }\n")
+
+    # N = min(max(N, 5), 10)
+    assert 5 <= lowest_butterworth_order <= 10
+
+    b, a = butter(
+        lowest_butterworth_order,
+        butterworth_natural_frequency,
+        btype=title, fs=sample_rate_hz
+    )
+
+    plt.figure(figsize=(12, 10))
+
+    filtered_signal = lfilter(b, a, signal_w_noise)
+
+    plt.subplot(2, 2, 1)
+    plt.plot(time_s, filtered_signal)
+    plt.title(f'Time Domain: Filtered Signal ({title})')
+    plt.xlabel('Time (s)')
+    plt.ylabel('Amplitude (V)')
+    plt.grid(True)
+
+    filtered_signal_fft = fft(filtered_signal) / time_s.shape[0]
+    filtered_signal_amps = np.abs(filtered_signal_fft)
+
+    plt.subplot(2, 2, 2)
+    plt.plot(fft_freqs[:len(fft_freqs)//2], filtered_signal_amps[:len(filtered_signal_amps)//2])
+    plt.title(f'Amplitude Spectrum: Filtered Signal ({title})')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Amplitude')
+    plt.grid(True)
+
+    # calculate frequency response (rads/sample)
+    f_resp_freqs_rad_p_s, filter_response = freqz(b, a, worN=8000)
+    # convert to Hz
+    f_resp_freqs_hz = f_resp_freqs_rad_p_s * sample_rate_hz / (2 * np.pi)
+
+    plt.subplot(2, 1, 2)
+    plt.plot(f_resp_freqs_hz, np.abs(filter_response))
+    plt.title(f'Frequency Response of the butterworth Filter ({title})')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Gain')
+    plt.grid(True)
+
+    plt.savefig(f"task_3_filter_{title}.png")
+    plt.tight_layout()
+    plt.show()
+
+    # omega = 2 * np.pi * cutoff_freq_hz / sample_rate_hz
+    # print(f"Cut-off frequecy H(w) ({cutoff_freq_hz:.2f} Hz) (dB):", 20 * np.log10(np.abs(H_omega(b, a, omega))))
+
+    compute_snr(noise, signal, "Original")
+    compute_snr(lfilter(b, a, noise), lfilter(b, a, signal), "Filtered")
+
+
+def process_and_save(filename, a, b, out_filename):
+    data, sample_rate = sf.read(filename)
+    output = lfilter(b, a, data)
+    wavfile.write(out_filename, sample_rate, output)
 
 
 def task_4():
-    pass
+    for (filename, ssss) in [
+        ("audio_8k.wav", [
+            (("lowpass", "lt450"), (450, 450*1.1)),
+            (("bandpass", "450_1000"), ((450*1.1, 1000), (450, 1000*1.1))),
+            (("bandpass", "1000_4000"), ((1000*1.05, 3900), (1000, 3900*1.05))),
+        ]), ("audio_44k.wav", [
+            (("lowpass", "lt450"), (450, 450*1.1)),
+            (("bandpass", "450_1000"), ((450*1.1, 1000), (450, 1000*1.1))),
+            (("bandpass", "1000_4000"), ((1000*1.15, 4000), (1000, 4000*1.15))),
+        ])
+    ]:
+        signal, sample_rate = sf.read(filename)
+        time_s = np.arange(len(signal)) / sample_rate
+        fft_freqs = fftfreq(time_s.shape[0], d=1/sample_rate)
+        
+        plt.figure(figsize=(12, 5))
+
+        plt.subplot(1, 2, 1)
+        plt.plot(time_s, signal)
+        plt.title(f'Time Domain: {filename}')
+        plt.xlabel('Time (s)')
+        plt.ylabel('Amplitude')
+        plt.grid(True)
+
+        signal_fft = fft(signal) / time_s.shape[0]
+        signal_amps = np.abs(signal_fft)
+
+        plt.subplot(1, 2, 2)
+        plt.plot(fft_freqs[:len(fft_freqs)//2], signal_amps[:len(signal_amps)//2])
+        plt.title('Amplitude Spectrum')
+        plt.xlabel('Frequency (Hz)')
+        plt.ylabel('Amplitude')
+        plt.grid(True)
+
+        plt.savefig(f"task_4_signal_{filename[:-4]}.png")
+        plt.tight_layout()
+        plt.show()
+
+        for (title, p), (wp, ws) in ssss:
+            # if "1000_4000" == p and filename == "audio_8k.wav":
+            #     continue
+            print(f"\n\n\nFilter: {title}")
+            print(f"wp {wp}, ws: {ws}")
+            lowest_butterworth_order, butterworth_natural_frequency = cheb1ord(
+                wp=wp,         # cut-off freq
+                ws=ws,         # stopband freq
+                gpass=3,
+                gstop=20,
+                analog=False, fs=sample_rate
+            )
+            print(f"{lowest_butterworth_order = }")
+            print(f"{butterworth_natural_frequency = }\n")
+
+            # N = min(max(N, 5), 10)
+            assert 5 <= lowest_butterworth_order <= 10
+
+            b, a = cheby1(
+                lowest_butterworth_order,
+                3,
+                butterworth_natural_frequency,
+                btype=title, fs=sample_rate
+            )
+
+            plt.figure(figsize=(12, 10))
+
+            filtered_signal = lfilter(b, a, signal)
+
+            wavfile.write(f"out_{title}_{p}_{filename}", sample_rate, filtered_signal)
+
+            plt.subplot(2, 2, 1)
+            plt.plot(time_s, filtered_signal)
+            plt.title(f'Time Domain: Filtered Signal ({title} {p})')
+            plt.xlabel('Time (s)')
+            plt.ylabel('Amplitude (V)')
+            plt.grid(True)
+
+            filtered_signal_fft = fft(filtered_signal) / time_s.shape[0]
+            filtered_signal_amps = np.abs(filtered_signal_fft)
+
+            plt.subplot(2, 2, 2)
+            plt.plot(fft_freqs[:len(fft_freqs)//2], filtered_signal_amps[:len(filtered_signal_amps)//2])
+            plt.title(f'Amplitude Spectrum: Filtered Signal ({title} {p})')
+            plt.xlabel('Frequency (Hz)')
+            plt.ylabel('Amplitude')
+            plt.grid(True)
+
+            # calculate frequency response (rads/sample)
+            f_resp_freqs_rad_p_s, filter_response = freqz(b, a, worN=8000)
+            # convert to Hz
+            f_resp_freqs_hz = f_resp_freqs_rad_p_s * sample_rate / (2 * np.pi)
+
+            plt.subplot(2, 1, 2)
+            plt.plot(f_resp_freqs_hz, np.abs(filter_response))
+            plt.title(f'Frequency Response of the chebyshev Filter ({title} {p})')
+            plt.xlabel('Frequency (Hz)')
+            plt.ylabel('Gain')
+            plt.grid(True)
+
+            plt.savefig(f"task_4_{filename[:-4]}_filter_{title}_{p}.png")
+            plt.tight_layout()
+            plt.show()
 
 
 def main():
     # task_1()
-    task_2()
+    # task_2()
     # task_3()
-    # task_4()
+    task_4()
 
 if __name__ == "__main__":
     main()
